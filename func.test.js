4150f173155e4e348101982afb56f9a953fd5a61
Adam Thometz <adamthometz@gmail.com>
2021-08-23 20:30:23 -0400

    updated test

--- func.test.js
+++ func.test.js
@@ -1,4 +1,4 @@
-const { getMean, getMedian, getMode } = require("./func")
+const { getMean, getMedian, getMode, getAll, parseNums } = require("./func")
 
 describe('test operations', () => {
   let nums;
@@ -6,16 +6,30 @@ describe('test operations', () => {
     nums = [1,2,3,4,5,6,7,7]
   })
 
-  test('test mean function', () => {
+  test('test parseNums function', () => {
+    const toParse = '1,2,3,4,5,6,7,7'
+
+    expect(parseNums(toParse)).toEqual(nums)
+  })
+
+  test('test getMean function', () => {
     expect(getMean(nums)).toEqual(4.375)
   })
-  test('test median function', () => {
+  test('test getMedian function', () => {
     // when nums.length is even
     expect(getMedian(nums)).toEqual(4.5)
     // when nums.length is odd
     expect(getMedian([1,2,3,4,5,6,7])).toEqual(4)
   })
-  test('test mode function', () => {
+  test('test getMode function', () => {
     expect(getMode(nums)).toEqual(7)
+  })
+  test('test getAll function', () => {
+    const answer = {
+      mean: 4.375,
+      median: 4.5,
+      mode: 7
+    }
+    expect(getAll(nums)).toEqual(answer)
   })
 })
